34cf08b55324690c32866d166cb37790f7b6e28d
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2024-05-14 02:29:16 +0400

    intro to react

---
+++ intro-to-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ReciptTitle from "./components/ReciptTitle"
+
+const App = () => {
+  return (
+    <article>
+      <h1>Recipe Manager</h1>
+      <ReciptTitle />
+    </article>
+  )
+}
+
+export default App
---
+++ intro-to-react/src/components/ReciptTitle.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const ReciptTitle = () => {
+  const title = "Mashed potatoes"
+  return <h2>{title}</h2>
+}
+
+export default ReciptTitle
